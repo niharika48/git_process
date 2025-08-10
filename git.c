1f9d35089b3748646a8d89f13996b584c99b6445
niharika48 <niharikasiri2006@gmail.com>
2025-08-10 21:53:23 +0530

    used for loop for this code

--- git.c
+++ git.c
@@ -6,4 +6,18 @@
 void generatePassword(int length) {
     char characters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
     int charSize = strlen(characters);
+char password[length + 1]; // +1 for null terminator
 
+    for (int i = 0; i < length; i++) {
+        password[i] = characters[rand() % charSize];
+    }
+    password[length] = '\0'; // Null-terminate the string
+
+    printf("Generated Password: %s\n", password);
+}
+
+int main() {
+    int length;
+
+    printf("Enter desired password length: ");
+    scanf("%d", &length);
